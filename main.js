27647029709b55d0f410fb7a98aa5f6d02a52a8e
Angus Munro <angusmunro12@gmail.com>
2023-02-01 22:51:05 +1100

    Adding basic layout styling, typography and partial navbar dropdown functionality

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+	// Navbar Hamburger Menu
+
+	$(".hamburger").on("click", mobileMenu);
+
+	function mobileMenu() {
+		$(".hamburger").toggleClass("active");
+		$(".navbar__menu").toggleClass("active");
+	}
+
+   $(".navbar__submenu-link").on("click", function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      $(".navbar__gallery-dropdown").toggleClass("active");
+   })
+
+	// $(".navbar__menu-link").forEach((n) => n.on("click", closeMenu));
+});
