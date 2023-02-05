2de8a150a6a8d74fd671674f26b297d7684496ff
Angus Munro <angusmunro12@gmail.com>
2023-02-05 19:43:08 +1100

    Adding 'before-after' styles and feature functionality

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	// Navbar Hamburger Menu
+	// || Navbar Hamburger Menu
 
 	$(".hamburger").on("click", mobileMenu);
 
@@ -8,11 +8,23 @@ $(document).ready(function () {
 		$(".navbar__menu").toggleClass("active");
 	}
 
-   $(".navbar__submenu-link").on("click", function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-      $(".navbar__gallery-dropdown").toggleClass("active");
-   })
+	$(".navbar__submenu-link").on("click", function (e) {
+		e.preventDefault();
+		e.stopPropagation();
+		$(".navbar__gallery-dropdown").toggleClass("active");
+	});
 
 	// $(".navbar__menu-link").forEach((n) => n.on("click", closeMenu));
+
+	// || Before-after Tab Toggle
+	$(".before-after-image, .before-after-button").on("click", function () {
+		clearInterval(imageTabAutoSwitcher);
+		imageTabToggle();
+	});
+
+	function imageTabToggle() {
+		$(".before-after-image").toggleClass("before-after-image--active");
+		$(".before-after-button").toggleClass("before-after-button--active");
+	}
+	const imageTabAutoSwitcher = setInterval(imageTabToggle, 4000);
 });
